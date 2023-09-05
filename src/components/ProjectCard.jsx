91781eb0904ee1ecf68ad6632252a19cd4f25649
deimov <cortezgonzalo@outlook.es>
2023-09-04 23:43:36 -0300

    add controls

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,8 +2,7 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { Cloudinary } from "@cloudinary/url-gen";
-import { useRef } from "react";
-import { useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 const Project = styled.div`
   margin: 0%;
@@ -54,29 +53,31 @@ const H3 = styled.h3`
   color: white;
   position: absolute;
   bottom: 25px;
+  
+
 `;
 
 const Video = styled.video`
-width: 440px;
+  width: 440px;
   height: 440px;
   margin-top: 0%;
-  
-`
+`;
 
-
+const DescriptionContainer = styled.div`
+   text-align: center;
+  margin-top: 10px;
+  color: white;
+  padding: 0 50px; /* Ajusta el valor del padding según sea necesario */
+  box-sizing: border-box; 
+`;
 
 const cld = new Cloudinary({
   cloud: {
-    cloudName: 'djkkxjn4u'
-  }
+    cloudName: "djkkxjn4u",
+  },
 });
 
-
-
-
 const ProjectCard = ({ id, title, category, date, description, type }) => {
-
-
   const videoRef = useRef(null);
 
   useEffect(() => {
@@ -89,10 +90,24 @@ const ProjectCard = ({ id, title, category, date, description, type }) => {
     }
   }, []);
 
-  
-
   const projectURL = `assets/${id}.jpg`;
   const projectVideo = `assets/videos/${id}.mp4`;
+
+  // Determina si la pantalla es lo suficientemente pequeña como para aplicar el estilo móvil
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 767); // Puedes ajustar este valor según tus necesidades
+
+  // Función para manejar el cambio de tamaño de la ventana
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 767);
+  };
+
+  // Agrega un evento de cambio de tamaño para verificar si cambia el estado móvil
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <motion.div
@@ -105,36 +120,32 @@ const ProjectCard = ({ id, title, category, date, description, type }) => {
           <Link
             onClick={() => handleClick("project-section")}
             to={`/project/${id}`}
-          > 
-          { type === 'image' && (
-             <IMG src={projectURL} />
-          )}
-           { type === 'video' && (
-
-             /*  <Video 
-              controls  
-              src={cld.video(`/assets/videos/${id}.mp4`).toURL}
-             /> */
-
-              
-
-            <Video
-            preload="auto"
-             ref={videoRef }
-             width="100%"
-             height= "100%"
-             playsInline
-             src={projectVideo} 
-             muted loop
-             />  
- 
-          )} 
- 
-              {/*  <IMG src={projectURL} />  */}
-          
-             
-            <TitleContainer>{<H3> {description} </H3>}</TitleContainer>
+          >
+            {type === "image" && <IMG src={projectURL} />}
+            {type === "video" && (
+              <Video
+                preload="auto"
+                controls
+                ref={videoRef}
+                width="100%"
+                height="100%"
+                playsInline
+                src={projectVideo}
+                muted
+                loop
+              />
+            )}
+            {isMobile && (
+              <DescriptionContainer>
+                <H3>{description}</H3>
+              </DescriptionContainer>
+            )}
           </Link>
+          {!isMobile && (
+            <TitleContainer>
+              <H3>{description}</H3>
+            </TitleContainer>
+          )}
         </ImageContainer>
       </Project>
     </motion.div>
@@ -142,10 +153,3 @@ const ProjectCard = ({ id, title, category, date, description, type }) => {
 };
 
 export default ProjectCard;
-
-
- {/* <Video src={cld.video(projectVideo).toURL}>
-              
-              Tu navegador no soporta el elemento de video.
-            </Video> */}
-          
