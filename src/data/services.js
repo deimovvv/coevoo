50db39b1b6962551669c891a3a40514a91befbc8
deimov <cortezgonzalo@outlook.es>
2024-01-10 22:29:17 -0300

    jajaj

--- src/data/services.js
+++ src/data/services.js
@@ -1,28 +1,28 @@
 const services = [
 
 {   "id": 'XR',
-    "title": "DESARROLLOS EN VR/AR/MR/XR",
-    "description": "Descubre un nuevo mundo de experiencias digitales con nuestros  servicios en VR (Realidad Virtual), AR (Realidad Aumentada), MR (Realidad Mixta) y XR (Realidad Extendida). Desde mundos virtuales   inmersivos hasta la integración de información digital en el mundo  real, Coevo transforma ideas en experiencias únicas y envolventes. Explora cómo estas tecnologías revolucionarias pueden potenciar tu visión y conectar con tu audiencia de manera innovadora.",
+    "title": "DEVELOPMENTS IN VR/AR/MR/XR",
+    "description": "Explore a new realm of digital experiences with our services in VR (Virtual Reality), AR (Augmented Reality), MR (Mixed Reality), and XR (Extended Reality). From immersive virtual worlds to the integration of digital information into the real world, Coevo transforms ideas into unique and engaging experiences. Discover how these revolutionary technologies can enhance your vision and connect with your audience in innovative ways.",
     "category": "servicios"
 },
 {   
     "id": '3D',
     "title": "3D",
-    "description": "Transforma tus conceptos en realidad con nuestro servicio de modelado y animación en 3D. En Coevo, creamos representaciones visuales cautivadoras y animaciones dinámicas que elevan tu proyecto al siguiente nivel. Desde productos y personajes hasta entornos virtuales, nuestro equipo experto da vida a tus ideas con un toque tridimensional único, ofreciendo un impacto visual que perdura.",
+    "description": "Materialize your concepts into reality with our 3D modeling and animation service. At Coevo, we craft captivating visual representations and dynamic animations that elevate your project to the next level. From products and characters to virtual environments, our expert team breathes life into your ideas with a unique three-dimensional touch, delivering a visual impact that endures",
     "category": "servicios"
    
 },
 {   
     "id": 'ProduccionesAudiovisuales',
-    "title": "PRODUCCIONES AUDIOVISUALES",
-    "description": "Captura la esencia de tu mensaje con nuestras producciones audiovisuales de alta calidad. Desde vídeos promocionales y publicitarios hasta contenido narrativo cautivador, en Coevo convertimos tus ideas en historias visuales impactantes. Descubre cómo nuestro enfoque creativo y técnico puede llevar tu contenido audiovisual al siguiente nivel, cautivando a tu audiencia y dejando una impresión duradera.",
+    "title": "AUDIOVISUAL PRODUCTIONS",
+    "description": "Capture the essence of your message with our high-quality audiovisual productions. From promotional and advertising videos to captivating narrative content, at Coevo we turn your ideas into impactful visual stories.",
     "category": "servicios"
     
 },
 {   
     "id": 'ProgramacionCreativa',
-    "title": "DISEÑO Y PROGRAMACION CREATIVA",
-    "description": "Eleva tu presencia en línea con nuestro servicio integral de diseño y programación web creativa en Coevo. Fusionamos diseño estético con funcionalidad avanzada, creando sitios web cautivadores y efectivos. Desde la conceptualización creativa hasta la implementación técnica, nuestro equipo experto garantiza una experiencia web única que destaca tu marca y cautiva a tus visitantes",
+    "title": "WEB DESIGN AND CREATIVE PROGRAMMING",
+    "description": "Elevate your online presence with our comprehensive creative web design and programming service. We fuse aesthetic design with advanced functionality, creating captivating and effective websites. From creative conceptualization to technical implementation, our expert team ensures a unique web experience that highlights your brand and captivates your visitors.",
     "category": "servicios"
 
     
@@ -30,8 +30,8 @@ const services = [
 
 {   
     "id": 'environment',
-    "title": "ENTORNOS VIRTUALES",
-    "description": "Sumérgete en experiencias virtuales envolventes con nuestros servicios de creación de entornos virtuales. Desde mundos imaginarios hasta recreaciones de espacios reales, Coevo transforma ideas en entornos tridimensionales cautivadores. Descubre cómo podemos llevar tus proyectos a la realidad virtual y ofrecer experiencias inmersivas que impacten a tu audiencia.",
+    "title": "VIRTUAL ENVIRONMENTS",
+    "description": "Immerse yourself in immersive virtual experiences with our virtual environment creation services. From imaginary worlds to recreations of real spaces, Coevo transforms ideas into captivating three-dimensional environments. Discover how we can bring your projects to virtual reality and offer immersive experiences that impact your audience.",
     "category": "servicios"
 
     
